7c3d053b08dd2cc81c8b9f42ac6e931153d76d4c
montse4 <160216050+montse4@users.noreply.github.com>
2025-05-16 00:08:33 +0200

    intro

--- Galaga/main.cpp
+++ Galaga/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-using namespace std;
 #include <raylib.h>
 #include "game.hpp"
 #include "space.hpp"
@@ -7,144 +6,166 @@ using namespace std;
 #include "Menu.hpp"
 #include "UI.hpp"
 #include "Transicion.hpp"
+using namespace std;
 
-
+// Definimos los diferentes estados del juego usando enum
+enum GameState {
+    STATE_INTRO,
+    STATE_INTRO_ANIMATION,
+    STATE_MENU,
+    STATE_TRANSITION,
+    STATE_GAMEPLAY,
+    STATE_GAMEOVER
+};
 
 int main() {
+    int initWidth = 896;
+    int initHeight = 928;
+    InitWindow(initWidth, initHeight, "Galaga");
+    SetTargetFPS(60);
+    InitAudioDevice();
 
-	// color gris oscuro
-	int initWidth = 896; // ancho de la pantalla
-	int initHeight = 928; // alto de la pantalla
+    Texture2D background_image = LoadTexture("resources/screens/bg_stage1_2.png");
+    Texture2D gameOverScreen = LoadTexture("resources/screens/gameOverScreen.png");
+    Texture2D intro = LoadTexture("resources/screens/intro-sheet.png");
 
-
-
-	InitWindow(initWidth, initHeight, "Galaga ");
-	SetTargetFPS(60); 
-	InitAudioDevice();
-	Texture2D background_image= LoadTexture("resources/screens/bg_stage1_2.png");
-	Texture2D gameOverScreen = LoadTexture("resources/screens/gameOverScreen.png");
-	Texture2D splashScreen = LoadTexture("resources/screens/splash.png");
-	
-	float scale = 4.0f; //aumentamos las dimensiones de la imagen de fondo
-	Vector2 position = { (initWidth - background_image.width * scale) / 2,  // Centrado horizontal
-	(initHeight - background_image.height * scale) / 2  // Centrado vertical
-	}; //posicionamos el fondo en el centro
-
-	float fadeAlpha = 1.0f;
-	bool FadingIn = false;
+    Texture2D introAnimation = LoadTexture("resources/screens/intro-sheet.png");
 
 
-	 //
-	Game game; // crea un objeto de la clase Game	
-	Menu menu;
-	GameUI ui;
-	Spaceship space;
-	
-	bool inTransition = false;
-	Transition transition;
+    int frameWidth = introAnimation.width / 2;
+    int frameHeight = introAnimation.height;
+    int currentFrame = 0;
+    int framesCounter = 0;
+    int framesSpeed = 8;
 
-	float gameOverTimer = 0.0f;
-    const float gameOverDelay = 15.0f; // 15 segundos
+    float scale = 4.0f;
+    Vector2 position = { (initWidth - background_image.width * scale) / 2, (initHeight - background_image.height * scale) / 2 };
+    Vector2 animationPosition;
+
+    float fadeAlpha = 1.0f;
+    bool FadingIn = false;
+
+    Game game;
+    Menu menu;
+    GameUI ui;
+    Spaceship space;
+    Transition transition;
+
+    float gameOverTimer = 0.0f;
+    const float gameOverDelay = 15.0f;
     bool gameOver = false;
 
+    GameState currentState = STATE_INTRO;
 
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
 
-    while (WindowShouldClose()==false) {
+        switch (currentState) {
+        case STATE_INTRO:
+           
+            DrawTextureEx(intro, position, 0.0f, scale, WHITE);
 
+            if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                currentState = STATE_INTRO_ANIMATION;
+                framesCounter = 0;
+                currentFrame = 0;
+            }
+            break;
 
+        case STATE_INTRO_ANIMATION:
+            
+            DrawTextureEx(intro, position, 0.0f, scale, WHITE);
 
-		if (game.getSpaceship().lives <= 0 && !gameOver) {
-			gameOver = true;
-			gameOverTimer = 0.0f;
-		}
+           
+            animationPosition.x = (initWidth - frameWidth * scale) / 2;
+            animationPosition.y = (initHeight - frameHeight * scale) / 2;
 
-		if (gameOver) 
-		{
-			gameOverTimer += GetFrameTime();
-			if (gameOverTimer >= gameOverDelay || IsKeyPressed(KEY_SPACE)) 
-			{
-				gameOver = false;
-				gameOverTimer = 0.0f;
-				menu = Menu();
-				menu.SetShowIntro(true);
-				menu.Reset();
-				inTransition = false;
-				FadingIn = false;
-				fadeAlpha = 1.0f;
-				game = Game();
-				game.getSpaceship().Reset();
-			}
-		}
-		else if (!menu.SingleModeSelected())
-		{
-			menu.Update();
-		}
-		else if (!inTransition)
-		{
-			inTransition = true;
+           
+            framesCounter++;
+            if (framesCounter >= (60 / framesSpeed)) {
+                framesCounter = 0;
+                currentFrame++;
 
-		}
-		else if (!transition.IsFinished())
-		{
-			transition.Update();
-			
-		}
-		else
-		{
-			if(game.getSpaceship().lives > 0)
-			{
-				if (!FadingIn)
-				{
-					FadingIn = true;
-				}
+                
+                if (currentFrame > 1) {  
+                    currentState = STATE_MENU;
+                }
+            }
 
-				if (fadeAlpha > 0.0f)
-				{
-					fadeAlpha -= 0.7f * GetFrameTime();
-					if (fadeAlpha < 0.0f)fadeAlpha = 0.0f;
-				}
-			}
-			game.Update();
-		}
-		
-		BeginDrawing(); 
-		ClearBackground(BLACK);
+            
+            {
+                Rectangle sourceRec = { currentFrame * frameWidth, 0, frameWidth, frameHeight };
+                Rectangle destRec = { animationPosition.x, animationPosition.y, frameWidth * scale, frameHeight * scale };
+                Vector2 origin = { 0, 0 };
+                DrawTexturePro(introAnimation, sourceRec, destRec, origin, 0.0f, WHITE);
+            }
+            break;
 
-		if (game.getSpaceship().lives <= 0) {
-			Vector2 gameOverPos = { (GetScreenWidth() - gameOverScreen.width * scale) / 2, (GetScreenHeight() - gameOverScreen.height * scale) / 2 };
-			DrawTextureEx(gameOverScreen, gameOverPos, 0.0f, 4.0f, WHITE);
+        case STATE_MENU:
+            if (!menu.SingleModeSelected()) {
+                menu.Update();
+                menu.Draw();
+            }
+            else {
+                currentState = STATE_TRANSITION;
+            }
+            break;
 
-		}
-		else
-		{
-			if (!menu.SingleModeSelected())
-			{
-				menu.Draw();
-			}
-			else if (!transition.IsFinished())
-			{
-				transition.Draw();
-			}
-			else
-			{
+        case STATE_TRANSITION:
+            transition.Update();
+            transition.Draw();
+            if (transition.IsFinished()) {
+                currentState = STATE_GAMEPLAY;
+                FadingIn = false;
+                fadeAlpha = 1.0f;
+            }
+            break;
 
-				DrawTextureEx(background_image, position, 0.0f, scale, WHITE);
-				game.Draw();
-				ui.Draw();
+        case STATE_GAMEPLAY:
+            if (game.getSpaceship().lives <= 0) {
+                currentState = STATE_GAMEOVER;
+                gameOverTimer = 0.0f;
+            }
+            else {
+                game.Update();
+                DrawTextureEx(background_image, position, 0.0f, scale, WHITE);
+                game.Draw();
+                ui.Draw();
+                if (!FadingIn && fadeAlpha > 0.0f) {
+                    fadeAlpha -= 0.7f * GetFrameTime();
+                    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, fadeAlpha));
+                    if (fadeAlpha <= 0.0f) FadingIn = true;
+                }
+            }
+            break;
 
-				if (fadeAlpha > 0.0f)
-				{
-					DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, fadeAlpha));
-				}
-			}
-		}
+        case STATE_GAMEOVER:
+            Vector2 gameOverPos = { (GetScreenWidth() - gameOverScreen.width * scale) / 2, (GetScreenHeight() - gameOverScreen.height * scale) / 2 };
+            DrawTextureEx(gameOverScreen, gameOverPos, 0.0f, 4.0f, WHITE);
+            gameOverTimer += GetFrameTime();
+            if (gameOverTimer >= gameOverDelay || IsKeyPressed(KEY_SPACE)) {
+                currentState = STATE_MENU;
+                menu = Menu();
+                menu.SetShowIntro(true);
+                menu.Reset();
+                game = Game();
+                game.getSpaceship().Reset();
+                FadingIn = false;
+                fadeAlpha = 1.0f;
+            }
+            break;
+        }
 
-	
-		EndDrawing();
+        EndDrawing();
     }
 
-    
-    CloseWindow();
+  
+    UnloadTexture(background_image);
+    UnloadTexture(gameOverScreen);
+    UnloadTexture(intro);
+    UnloadTexture(introAnimation);
 
+    CloseWindow();
     return 0;
 }
